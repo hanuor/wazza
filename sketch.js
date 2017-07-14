46aadc713f32cb3c80a75c9a1c6f33aa783542d0
hanuor <hanuortech@gmail.com>
2017-07-14 13:46:56 +0530

    Random walker

--- sketch.js
+++ sketch.js
@@ -6,10 +6,15 @@ function setup(){
 function draw(){
 	background(51);
 	w.display();
+	w.walk();
 }
 function Walker(){
 	this.x = width/2;
 	this.y = height/2;
+	this.walk  = function(){
+		this.x = this.x + random(-13,13);
+		this.y = this.y + random(-13,13);
+	}
 	this.display = function(){
 		fill(255);
 		ellipse(this.x, this.y, 48, 48);
