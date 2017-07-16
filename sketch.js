171109872db7c1f50f3311fd01bb67eabfb81b57
hanuor <hanuortech@gmail.com>
2017-07-16 12:35:51 +0530

    Physics engine

--- sketch.js
+++ sketch.js
@@ -5,18 +5,23 @@ function setup(){
 }
 function draw(){
 	background(51);
+	w.update();
+
 	w.display();
-	w.walk();
 }
 function Walker(){
-	this.post = createVector(width/2, height/2);
+	this.post = createVector(width/2, 0);
+	this.vel = createVector(0,0);
+	this.acc = createVector(0,0.1);
+	 
+
 	// this.x = width/2;
 	// this.y = height/2;
 	
-	 this.walk  = function(){
-	 	this.post.x = this.post.x + random(-13,13);
-	 	this.post.y = this.post.y + random(-13,13);
-	 }
+	 this.update  = function(){
+	 	this.vel.add(this.acc);
+	 	this.post.add(this.vel);
+	 	}
 	this.display = function(){
 		fill(255);
 		ellipse(this.post.x, this.post.y, 48, 48);
